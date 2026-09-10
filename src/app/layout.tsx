e5a1abdb17865683932d9f105d4c3e41a07909d9
sgalanb <73245438+sgalanb@users.noreply.github.com>
2026-09-10 16:44:46 -0300

    add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,36 @@ import type { Metadata, Viewport } from "next";
 
 import "./globals.css";
 
+const TITLE = "basement.studio | We make cool shit that performs.";
+const DESCRIPTION =
+  "basement.studio is a digital studio crafting brands, websites, 3D experiences, and products. We design and engineer cool shit that actually performs.";
+
 export const metadata: Metadata = {
-  title: "basement.studio | We make cool shit that performs.",
-  description:
-    "basement.studio is a digital studio crafting brands, websites, 3D experiences, and products. We design and engineer cool shit that actually performs.",
+  title: TITLE,
+  description: DESCRIPTION,
+  openGraph: {
+    title: TITLE,
+    description: DESCRIPTION,
+    images: [
+      {
+        alt: TITLE,
+        height: 630,
+        url: "/opengraph-image.jpg",
+        width: 1200,
+      },
+    ],
+    locale: "en_US",
+    siteName: "basement.studio",
+    type: "website",
+    url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
+  },
+  twitter: {
+    title: TITLE,
+    description: DESCRIPTION,
+    site: "@basementstudio",
+    creator: "@basementstudio",
+    card: "summary_large_image",
+  },
 };
 
 export const viewport: Viewport = {
